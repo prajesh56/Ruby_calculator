7eef25ced3982727bf842c16f1f39b13f6d661ed
prajesh56 <pj.shakya10@gmail.com>
2022-02-08 18:38:52 +0545

    fix indent using space by 2

--- calculator_spec.rb
+++ calculator_spec.rb
@@ -1,11 +1,10 @@
 require 'rspec'
-require_relative 'calculator.rb'
+require'./calculator.rb'
 
  
 describe Calculator do
-    it 'creates a new class of Calculator' do
-        naya = Calculator.new (1)
-        
-        expect(naya).to be_kind_of(Calculator)
-    end
+  it 'creates a new class of Calculator' do
+    naya = Calculator.new (1)
+    expect(naya.class).to eql(Calculator)
+  end
 end
